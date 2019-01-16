13a971b0a02389e62ce1d6eb926bdaf9706935b1
Musteel <mustafa_9611@hotmail.com>
2019-01-16 09:42:55 +0100

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("kkkkkkkkkk2");
-        System.out.println("kkkkkkkkkk1");
+        System.out.println("kkkkkkkkkk23");
+        System.out.println("kkkkkkkkkk12");
     }
 }
