e40f3657945ff1961e11e6b039e9cf811bcbe5e2
Musteel <mustafa_9611@hotmail.com>
2019-01-16 09:38:55 +0100

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("KKKKKKKKKK");
-        System.out.println("KKKKKKKKKK");
+        System.out.println("xxxxxxxxxx");
         System.out.println("xxxxxxxxxx");
     }
 }
