700545f776eb5764e0aa36301be7a46e87b492ba
Musteel <mustafa_9611@hotmail.com>
2019-01-16 09:32:04 +0100

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("KKKKKKKKKK");
         System.out.println("KKKKKKKKKK");
-        System.out.println("AAAAAAAAAA");
+        System.out.println("xxxxxxxxxx");
     }
 }
