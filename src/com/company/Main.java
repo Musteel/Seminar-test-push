10fe4414a77e03af207ea4075f71d8596929286c
Musteel <mustafa_9611@hotmail.com>
2019-01-16 10:08:20 +0100

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("KKKKKKKKKK");
         System.out.println("KKKKKKKKKK");
-        System.out.println("AAAAAAAAAA");
     }
 }
